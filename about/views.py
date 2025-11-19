5d5869c3c769ae1bd1d3bc5f42daa7abbfc88e37
Alicia <alicia.westerlund@outlook.com>
2025-11-19 15:09:30 +0100

    Add gallery with images and About us dropdown pages with file structures

--- about/views.py
+++ about/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def about(request):
     return render(request, "about/about.html")
+
+def how_it_all_started(request):
+    return render(request, "about/how_it_all_started.html")
+
+def gallery(request):
+    return render(request, "about/gallery.html")
+
+def collaborations_with_farmers(request):
+    return render(request, "about/collaborations_with_farmers.html")
