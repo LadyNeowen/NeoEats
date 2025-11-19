6625bd689170d182b9f3a6ea92d393fa2525f6d7
Alicia <alicia.westerlund@outlook.com>
2025-11-19 15:09:30 +0100

    Add gallery with images and About us dropdown pages with file structures

--- about/urls.py
+++ about/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.about, name='about'),
+    
+    path('how-it-all-started/', views.how_it_all_started, name='about_story'),
+    path('gallery/', views.gallery, name='about_gallery'),
+    path('collaborations-with-farmers/', views.collaborations_with_farmers, name='about_collaborations'),
 ]
