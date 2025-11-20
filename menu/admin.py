42d75d252019da1c71fc9d17ca516609645696ea
Alicia <alicia.westerlund@outlook.com>
2025-11-20 16:42:03 +0100

    Add menu with style css

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import MenuItem
 # Register your models here.
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'vegetarian', 'gluten_free')
+    list_filter = ('category', 'vegetarian', 'gluten_free')
+    search_fields = ('name', 'description')
