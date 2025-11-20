6db83f1229de35fa2f231b67f9d217c2c1affe8b
Alicia <alicia.westerlund@outlook.com>
2025-11-20 17:22:14 +0100

    Updated menu.html with admin and views.py

--- menu/views.py
+++ menu/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from .models import MenuItem
 
 def menu(request):
-    return render(request, "menu/menu.html")
+    starters = MenuItem.objects.filter(category='starter')
+    mains = MenuItem.objects.filter(category='main')
+    desserts = MenuItem.objects.filter(category='dessert')
+
+    return render(request, "menu/menu.html", {
+        "starters": starters,
+        "mains": mains,
+        "desserts": desserts,
+    })
