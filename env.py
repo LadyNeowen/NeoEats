0def6e8c69f7b4fd67aa278431227e22b13bcd41
Alicia <alicia.westerlund@outlook.com>
2025-11-20 13:27:09 +0100

    Add SQL and env.py settings

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ.setdefault(
+    'DATABASE_URL', 'postgresql://neondb_owner:npg_fUAgP8EC1wZe@ep-withered-cell-ag25se60.c-2.eu-central-1.aws.neon.tech/boss_lunar_outer_601119'
+)
