9b4bb353a78959f244d0c561c8e324e11fd6f4d6
Alicia <alicia.westerlund@outlook.com>
2025-11-20 19:12:10 +0100

    Add admin edit functions for gallery and about pages

--- about/urls.py
+++ about/urls.py
@@ -3,8 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.about, name='about'),
-    
-    path('how-it-all-started/', views.how_it_all_started, name='about_story'),
-    path('gallery/', views.gallery, name='about_gallery'),
-    path('collaborations-with-farmers/', views.collaborations_with_farmers, name='about_collaborations'),
+    path('gallery/', views.about_gallery, name='about_gallery'),
+    path('collaborations-with-farmers/', views.about_collaborations, name='about_collaborations'),
 ]
